0522b82dfc3b266ea20bf265c85fbb0da4e1366c
tysontrail <tyson.trail@gmail.com>
2022-11-23 16:05:27 -0700

    updated student login

--- assignment6/src/main/java/course/ensf607/assignment6/student/StudentController.java
+++ assignment6/src/main/java/course/ensf607/assignment6/student/StudentController.java
@@ -1,28 +1,32 @@
 package course.ensf607.assignment6.student;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
-    private final StudentService studentService;
+  private final StudentService studentService;
 
-    @Autowired
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
+  @Autowired
+  public StudentController(StudentService studentService) {
+    this.studentService = studentService;
+  }
 
-    @GetMapping
-    public List<Student> getStudents() {
-        return studentService.getAllStudents();
-    }
+  @GetMapping
+  public List<Student> getStudents() {
+    return studentService.getAllStudents();
+  }
 
-    @PostMapping
-    public void registerNewStudent(@RequestBody Student student) {
-        studentService.addNewStudent(student);
-    }
+  @PostMapping("/register")
+  public void registerNewStudent(@RequestBody Student student) {
+    studentService.addNewStudent(student);
+  }
+
+  @PostMapping("/login")
+  public Student loginStudent(@RequestBody Student student) {
+    return studentService.loginStudent(student);
+  }
 }
