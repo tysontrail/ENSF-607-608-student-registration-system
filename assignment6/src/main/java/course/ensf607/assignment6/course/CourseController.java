c941b84605e886ea9d3f38472ca545d819853ee8
tysontrail <tyson.trail@gmail.com>
2022-11-23 15:15:06 -0700

    Updated

--- assignment6/src/main/java/course/ensf607/assignment6/course/CourseController.java
+++ assignment6/src/main/java/course/ensf607/assignment6/course/CourseController.java
@@ -2,43 +2,51 @@ package course.ensf607.assignment6.course;
 
 import course.ensf607.assignment6.student.Student;
 import course.ensf607.assignment6.student.StudentService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 
 @RestController
 @RequestMapping(path = "api/v1/course")
 public class CourseController {
 
-    private final CourseService courseService;
+  private final CourseService courseService;
 
-    private final StudentService studentService;
+  private final StudentService studentService;
 
-    @Autowired
-    public CourseController(CourseService courseService, StudentService studentService) {
-        this.courseService = courseService;
-        this.studentService = studentService;
-    }
+  @Autowired
+  public CourseController(CourseService courseService, StudentService studentService) {
+    this.courseService = courseService;
+    this.studentService = studentService;
+  }
 
-    @GetMapping
-    public List<Course> getAllCourses() {
-        return courseService.getAllCourses();
-    }
+  @GetMapping
+  public List<Course> getAllCourses() {
+    return courseService.getAllCourses();
+  }
 
-    @PostMapping
-    public void registerNewCourse(@RequestBody Course course) {
-        courseService.addNewCourse(course);
-    }
+  @PostMapping
+  public void registerNewCourse(@RequestBody Course course) {
+    courseService.addNewCourse(course);
+  }
 
-    @PutMapping("{courseId}/students/{studentId}")
-    public Course enrollStudentToCourse(@PathVariable Long courseId,
-                                         @PathVariable Long studentId) {
-        Course course = courseService.getCourseById(courseId);
-        Student student = studentService.getStudentById(studentId);
-        course.enrolledStudents(student);
-        courseService.updateCourse(course);
-        return course;
-    }
+  // @PutMapping("{courseId}/students/{studentId}")
+  // public Course enrollStudentToCourse(@PathVariable Long courseId,
+  //                                      @PathVariable Long studentId) {
+  //     Course course = courseService.getCourseById(courseId);
+  //     Student student = studentService.getStudentById(studentId);
+  //     course.enrolledStudents(student);
+  //     courseService.updateCourse(course);
+  //     return course;
+  // }
+
+  @PutMapping("{courseName}/students/{studentId}")
+  public Course enrollStudentToCourse(
+      @PathVariable String courseName, @PathVariable Long studentId) {
+    Course course = courseService.getCourseByName(courseName);
+    Student student = studentService.getStudentById(studentId);
+    course.enrolledStudents(student);
+    courseService.updateCourse(course);
+    return course;
+  }
 }
--- assignment6/src/main/java/course/ensf607/assignment6/course/CourseService.java
+++ assignment6/src/main/java/course/ensf607/assignment6/course/CourseService.java
@@ -1,42 +1,50 @@
 package course.ensf607.assignment6.course;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
 
-    private final CourseRepository courseRepository;
+  private final CourseRepository courseRepository;
 
-    @Autowired
-    public CourseService(CourseRepository courseRepository) {
-        this.courseRepository = courseRepository;
-    }
+  @Autowired
+  public CourseService(CourseRepository courseRepository) {
+    this.courseRepository = courseRepository;
+  }
 
-    public List<Course> getAllCourses() {
-        return courseRepository.findAll();
-    }
+  public List<Course> getAllCourses() {
+    return courseRepository.findAll();
+  }
 
-    public void addNewCourse(Course course) {
-        Optional<Course> courseByName = courseRepository.findByName(course.getName());
-        if (courseByName.isPresent()) {
-            throw new IllegalStateException("Course already exist!");
-        }
-        courseRepository.save(course);
+  public void addNewCourse(Course course) {
+    Optional<Course> courseByName = courseRepository.findByName(course.getName());
+    if (courseByName.isPresent()) {
+      System.out.println(courseByName);
+      throw new IllegalStateException("Course already exist!");
     }
+    courseRepository.save(course);
+  }
 
-    public void updateCourse(Course course) {
-        courseRepository.save(course);
-    }
+  public void updateCourse(Course course) {
+    courseRepository.save(course);
+  }
 
-    public Course getCourseById(Long courseId) {
-        Optional<Course> courseById = courseRepository.findById(courseId);
-        if (!courseById.isPresent()) {
-            throw new IllegalStateException("Course does'nt exist!");
-        }
-        return courseById.get();
+  public Course getCourseById(Long courseId) {
+    Optional<Course> courseById = courseRepository.findById(courseId);
+    if (!courseById.isPresent()) {
+      throw new IllegalStateException("Course doesn't exist!");
     }
+    return courseById.get();
+  }
+
+  public Course getCourseByName(String courseName) {
+    Optional<Course> courseByName = courseRepository.findByName(courseName);
+    if (!courseByName.isPresent()) {
+      throw new IllegalStateException("Course doesn't exist!");
+    }
+    return courseByName.get();
+  }
 }
